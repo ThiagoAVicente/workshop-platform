c1cbdcd6904f12c76d7c83e26abdd2daa28007b1
vcnt <thiago.vicente@ua.pt>
2026-02-25 17:33:56 +0000

    fix: update S3 backend bucket name to match actual bucket

--- platform/backend.tf
+++ platform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "workshop-ua-terraform-state-bucket"
+    bucket         = "workshop-ua-dev-terraform-state-092568126530"
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-state-locks"
